dbc5725c4213c58ed113e8480bab4d9f6abc6358
Scott Miller <scott.miller171@gmail.com>
2025-06-11 13:28:23 -0400

    init

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,54 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { Booklet, BookletPage } from "@millergeek/vue-library-printable-zine";
+import { useStorage } from '@vueuse/core'
+
+const pages = useStorage(
+  'pages',
+  Array.from({ length: 8 }, (_x, i) => ({
+  pageNumber: i+1,
+  imgData: ""
+}))
+)
+
+const onFileChange = (page, event) => {
+  const files = event.target.files || event.dataTransfer.files;
+  if (!files.length) {
+    return
+  }
+  const reader = new FileReader()
+  reader.onload = () => {
+    page.imgData = reader.result
+  }
+  reader.readAsDataURL(event.target.files[0])
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+<Booklet>
+  <BookletPage
+    v-for="(page, i) in pages"
+    :pageNumber="i+1"
+    :key="'bookPage'+(i+1)"
+    :style="{backgroundImage: 'url(' + page.imgData + ')'}"
+  >
+    <form>
+      <input class="noPrint" accept="image/*" type='file' @change="onFileChange(page, $event)" name="test" />
+      <button class="noPrint" @click="page.imgData = ''">Remove image</button>
+    </form>
+  </BookletPage>
+</Booklet>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+@media print {
+  .noPrint {
+    display: none;
+  }
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.booklet-page {
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+  background-clip: padding-box;
 }
 </style>
